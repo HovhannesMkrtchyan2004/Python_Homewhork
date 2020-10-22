f029adca330b38094562c1f7e1d0db9dcbcc2c4f
Hovhannes Mkrtchyan <hovhannes.m5.g@gmail.com>
2020-10-23 02:10:30 +0400

    Tnayin

--- Lesson 30.py
+++ Lesson 30.py
@@ -26,3 +26,77 @@
 #
 # print({el: string.count(el) for el in vow})
 # print({el: string.count(el) for el in con})
+
+# Narek 1
+# x = [5, 3, 6, 7, 9]
+# i = 2
+# qayl = 0
+# while i < max(x) + 1:
+#     if qayl not in x:
+#         qayl += i
+#         if qayl > max(x):
+#             print(i)
+#             break
+#         continue
+#     qayl = 0
+#     i += 1
+
+# Narek 2
+x = input().split('=')
+y = list(x[0])
+for i in range(len(x[0])):
+    if x[0][i] == '+':
+        x[1] += '-'
+        j = i + 1
+        while j < len(x[0]) and x[0][j].isdigit():
+            x[1] += x[0][j]
+            j += 1
+    elif x[0][i] == '-':
+        x[1] += '+'
+        j = i + 1
+        while j < len(x[0]) and x[0][j].isdigit():
+            x[1] += x[0][j]
+            j += 1
+
+count = 0
+for i in range(len(x[1]) - 1, -1, -1):
+    if x[1][i] == '+':
+        j = i + 1
+        z = ''
+        while j < len(x[1]) and x[1][j].isdigit():
+            z += x[1][j]
+            j += 1
+        if len(z)>0:
+            count += int(z)
+    elif x[1][i] == '-':
+        j = i + 1
+        z = ''
+        while j < len(x[1]) and x[1][j].isdigit():
+            z += x[1][j]
+            j += 1
+        if len(z)>0:
+            count -= int(z)
+    elif i == 0 and x[1][i].isdigit():
+        j = i
+        z = ''
+        while j < len(x[1]) and x[1][j].isdigit():
+            z += x[1][j]
+            j += 1
+        count += + int(z)
+count_x = 0
+for i in range(len(x[0])):
+    if x[0][i] == 'x':
+        if i == 0:
+            count_x += 1
+        elif x[0][i-1].isdigit():
+            j = i - 1
+            z = ''
+            while j >= 0 and x[1][j].isdigit():
+                z += x[0][j]
+                j -= 1
+            if len(z):
+                count_x += int(z)
+        else:
+            count_x += 1
+
+print('x =', int(count/count_x))
