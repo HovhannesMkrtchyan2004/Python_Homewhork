8732108e3e0fd0d89f822e8e3a6fcaf94bd85435
Hovhannes Mkrtchyan <hovhannes.m5.g@gmail.com>
2020-11-06 13:41:43 +0400

    Tnayin

--- Lesson 34.py
+++ Lesson 34.py
@@ -32,3 +32,64 @@
 #
 #
 # print(mult([-3, -5, -2, 20,1,33,-500]))
+
+# Ruben 1
+# def answer_queries(k, *query_counts):
+#     count = 1
+#     summ = sum(query_counts)
+#     while summ >= k:
+#         summ -= k
+#         count += 1
+#     return  count
+#
+#
+# print(answer_queries(1, 100))
+
+# Ruben 2
+def non_decreasing_sequence(*nums):
+    # flag = False
+    nums = list(nums)
+    for i in range(2, len(nums)):
+        if nums[i - 2] <= nums[i - 1] <= nums[i] and nums == sorted(nums):
+            return True, nums
+        if nums[i - 2] <= nums[i - 1] > nums[i]:
+            if nums[i - 2] <= nums[i - 1] <= -nums[i] or -nums[i - 2] <= -nums[i - 1] <= nums[i]:
+                nums[i] = -nums[i]
+                if nums == sorted(nums):
+                    return True, nums
+                elif not nums[i - 2] <= nums[i - 1] <= nums[i]:
+                    nums[i] = -nums[i]
+                nums[i - 1] = -nums[i - 1]
+                if nums == sorted(nums):
+                    return True, nums
+                elif not nums[i - 1] <= nums[i]:
+                    nums[i - 1] = -nums[i - 1]
+                nums[i - 2] = -nums[i - 2]
+                if nums == sorted(nums):
+                    return True, nums
+                elif not nums[i - 2] <= nums[i - 1]:
+                    nums[i - 2] = -nums[i - 2]
+        if nums[i - 2] > nums[i - 1] > nums[i]:
+            if -nums[i - 2] <= -nums[i - 1] <= -nums[i]:
+                nums[i - 2], nums[i - 1], nums[i] = -nums[i - 2], -nums[i - 1], -nums[i]
+                if nums == sorted(nums):
+                    return True, nums
+                elif not nums[i - 2] <= nums[i - 1] <= nums[i]:
+                    nums[i - 2], nums[i - 1], nums[i] = -nums[i - 2], -nums[i - 1], -nums[i]
+        if nums[i - 2] > nums[i - 1] < nums[i]:
+            if -nums[i - 2] <= nums[i - 1] <= nums[i] or nums[i - 2] <= -nums[i - 1] <= nums[i]:
+                nums[i - 2] = -nums[i - 2]
+                if nums == sorted(nums):
+                    return True, nums
+                elif not nums[i - 2] <= nums[i - 1] <= nums[i]:
+                    nums[i - 2] = -nums[i - 2]
+                nums[i - 1] = -nums[i - 1]
+                if nums == sorted(nums):
+                    return True, nums
+                elif not nums[i - 2] <= nums[i - 1] <= nums[i]:
+                    nums[i - 1] = -nums[i - 1]
+
+    return False
+
+
+print(non_decreasing_sequence(  1, -1, -2, 3, 6,-7))
