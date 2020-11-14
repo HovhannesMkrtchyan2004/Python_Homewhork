03de2735028b28e29596668c4c62201f8c818eb5
Hovhannes Mkrtchyan <hovhannes.m5.g@gmail.com>
2020-11-14 18:22:34 +0400

    Tnayin

--- Directory/create_and_delete_dirs_and_files.py
+++ Directory/create_and_delete_dirs_and_files.py
@@ -37,13 +37,13 @@ def create_a_testing_work_environment(initial_directory_path):
         filepath = os.path.join(new_dir, 'file_' + str(i) + '.txt')
         with open(filepath, 'w'):
             pass
-    new_dir = os.path.join(initial_directory_path, os.path.join('dir_2', 'dir_7'))
+    new_dir = os.path.join(initial_directory_path, os.path.join('dir_2','dir_7'))
     filepath = os.path.join(new_dir, 'file_' + '11' + '.txt')
     with open(filepath, 'w'):
         pass
 
 
-# create_a_testing_work_environment(initial_directory_path)
+#create_a_testing_work_environment(initial_directory_path)
 
 
 def delete_all_the_files_and_directories_recursively(directory):
@@ -54,5 +54,6 @@ def delete_all_the_files_and_directories_recursively(directory):
         elif os.path.isdir(new_path):
             delete_all_the_files_and_directories_recursively(new_path)
             os.rmdir(new_path)
+
 
 # delete_all_the_files_and_directories_recursively(initial_directory_path)
